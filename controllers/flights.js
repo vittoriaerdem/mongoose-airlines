a0a7b6cd1f1c5411a4c32a6041867a97c7ce471f
Vittoria Erdem <vittoriaerdem@gmail.com>
2019-08-19 13:53:22 -0500

    add stylesheet

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,3 +1,10 @@
+var Flight = require('../models/flight');
+var Destinations = require('../models/flight');
+
+module.exports = {
+    new: newFlight,
+  };
+
 function newFlight(req, res) {
     res.render('flights/new', { title: 'Add Flight' });
   }
--- routes/flights.js
+++ routes/flights.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 var flightsCtrl = require('../controllers/flights');
 
-router.get('/new', flightsCtrl.new);
+router.get('/flight', flightsCtrl.new);
 
 module.exports = router;
