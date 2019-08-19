afc2a60c87d172c5117c666948b4975bed76710f
Vittoria Erdem <vittoriaerdem@gmail.com>
2019-08-19 13:55:37 -0500

    add views/partials folder along with header and footer files

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,10 +1,43 @@
+
 var Flight = require('../models/flight');
 var Destinations = require('../models/flight');
+var Ticket = require('../models/ticket');
 
 module.exports = {
-    new: newFlight,
-  };
+  index,
+  show,
+  new: newFlight,
+  create
+};
+
+function index(req, res) {
+  Flight.find({}, function(err, flights) {
+    res.render('flights/index', { title: 'All Flights', flights});
+  });
+}
+
+function show(req, res) {
+  Flight.findById(req.params.id)
+  .populate('destination').exec(function(err, flight) {
+    Ticket.find({_id: {$nin: flight.destination}})
+    Destinations.find({_id: {$nin: flight.destination}})
+    .exec(function(err, tickets) {
+      // console.log(tickets);
+      res.render('flights/show', {
+        title: 'Flight Details', flight, tickets
+      });
+    });
+  });
+}
 
 function newFlight(req, res) {
-    res.render('flights/new', { title: 'Add Flight' });
-  }
+  res.render('flights/new', { title: 'Add Flight' });
+}
+
+function create(req, res) {
+  var flight = new Flight(req.body);
+  flight.save(function(err) {
+    if (err) return res.redirect('/flights');
+    res.redirect('/flights');
+  });
+}
